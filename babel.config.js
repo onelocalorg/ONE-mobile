539f745d5075d48723778917636b3f792b18d97f
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-04-15 10:37:52 -0500

    Upgrade babel and enable api caching
    Needed to fix a bug that causes dotenv to fail (per docs)

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,30 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    [
-      require.resolve("babel-plugin-module-resolver"),
-      {
-        cwd: "babelrc",
-        extensions: [".ts", ".tsx", ".js", ".ios.js", ".android.js"],
-        alias: {
-          "@assets": "./src/assets",
-          "@theme": "./src/theme",
-          "@constants": "./src/assets/constants",
-          "@images": "./src/assets/images",
-          "@components": "./src/components",
-          "@screens": "./src/screens",
-          "@config": "./src/config",
-          "@utils": "./src/utils",
-          "@network": "./src/network",
-          "@app-hooks": "./src/app-hooks",
-          "@store": "./src/store",
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        require.resolve("babel-plugin-module-resolver"),
+        {
+          cwd: "babelrc",
+          extensions: [".ts", ".tsx", ".js", ".ios.js", ".android.js"],
+          alias: {
+            "@assets": "./src/assets",
+            "@theme": "./src/theme",
+            "@constants": "./src/assets/constants",
+            "@images": "./src/assets/images",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@config": "./src/config",
+            "@utils": "./src/utils",
+            "@network": "./src/network",
+            "@app-hooks": "./src/app-hooks",
+            "@store": "./src/store",
+          },
         },
-      },
+      ],
+      "jest-hoist",
+      "react-native-reanimated/plugin",
     ],
-    "jest-hoist",
-    "react-native-reanimated/plugin",
-  ],
+  };
 };
