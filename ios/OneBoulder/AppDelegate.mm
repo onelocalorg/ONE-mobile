872c69df0891fb198e40e411afec56111b01c35e
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-05-05 11:04:10 -0500

    fix(maps): Upgrade Mapbox to fix zoom pinch on iOS

--- ios/OneBoulder/AppDelegate.mm
+++ ios/OneBoulder/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyCSzaTr_oKw3-wYT3PhWgErlx7St9UBB8M"];
+  [GMSServices provideAPIKey:@"AIzaSyCobkVCxli93gBohNPhJhuHBoWThs1pZlo"];
   self.moduleName = @"OneBoulder";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
