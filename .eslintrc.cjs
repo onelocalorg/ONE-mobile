0809c956aadc32dddbf73cdb87041938fb9489b8
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-06-14 19:43:22 -0500

    chore: Install eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    "react-native/react-native": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    // "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  plugins: ["react", "react-native", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  overrides: [
+    {
+      // To fix warning in the .eslintrc.cjs file
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: [".eslintrc.cjs"],
+    },
+  ],
+};
