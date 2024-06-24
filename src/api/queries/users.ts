7b8bec64e8bb924c2e17b9f12abb4a2005a03caa
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-06-24 13:11:36 -0500

    chore: Remove unused files

--- src/api/queries/users.ts
+++
@@ -1,8 +0,0 @@
-import { queryOptions } from "@tanstack/react-query";
-
-export function userProfile(id: string) {
-  return queryOptions({
-    queryKey: ["getUserProfile", id],
-    queryFn: () => getUserProfile(id),
-  });
-}
--- src/app-hooks/ApiContext.tsx
+++
@@ -1,3 +0,0 @@
-export function ApiProvider() {
-  const accessToken = useAccessToken();
-}
