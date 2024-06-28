1b25b97101b0041320b8f8b5848667d3b6b80b21
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-06-28 10:00:04 -0500

    chore: Bump version to x.0.1

--- src/network/constant.ts
+++ src/network/constant.ts
@@ -52,5 +52,5 @@ export const apiKeys = {
   packageDetail: "packageDetails",
 };
 
-export const IOS_VERSION = "8.0.0";
-export const ANDROID_VERSION = "2.0.0";
+export const IOS_VERSION = "8.0.1";
+export const ANDROID_VERSION = "2.0.1";
