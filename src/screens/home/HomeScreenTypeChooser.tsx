35bb836e49af30aaa502b112a45bbd5ef3b7ed96
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-01-17 12:13:59 +0800

    feat(group): Group selector on home

--- src/screens/home/HomeScreenTypeChooser.tsx
+++ src/screens/home/HomeScreenTypeChooser.tsx
@@ -1,45 +1,42 @@
+import { Rows4, Users } from 'lucide-react-native';
 import React from "react";
-import { Pressable, Text, View } from "react-native";
-import { useAppTheme } from "~/app-hooks/use-app-theme";
-import { createStyleSheet } from "./style";
+import {
+  Button,
+  ButtonIcon,
+  ButtonText
+} from "~/components/ui/button";
+import { Center } from '~/components/ui/center';
+import { HStack } from '~/components/ui/hstack';
 
 export const HomeScreenTypeChooser = ({
   onGroupsChosen,
 }: {
   onGroupsChosen: (isGroupsChosen: boolean) => void;
 }) => {
-  const { theme } = useAppTheme();
-  const styles = createStyleSheet(theme);
   const [isGroupsChosen, setGroupsChosen] = React.useState(false);
 
   const notifySetGroupsChosen = (newSetting: boolean) => {
+    console.log('click notify')
     if (isGroupsChosen !== newSetting) {
       onGroupsChosen(newSetting);
       setGroupsChosen(newSetting);
     }
   };
 
-  const postsButtonStyle = isGroupsChosen
-    ? styles.button
-    : { ...styles.button, ...styles.buttonPressed };
-  const groupsButtonStyle = isGroupsChosen
-    ? { ...styles.button, ...styles.buttonPressed }
-    : styles.button;
-
   return (
-    <>
-      <View style={styles.buttonContainer}>
-        <Pressable onPress={() => notifySetGroupsChosen(false)}>
-          <View style={postsButtonStyle}>
-            <Text style={styles.buttonText}>Posts</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => notifySetGroupsChosen(true)}>
-          <View style={groupsButtonStyle}>
-            <Text style={styles.buttonText}>Groups</Text>
-          </View>
-        </Pressable>
-      </View>
-    </>
+      <Center>
+       <HStack>
+        <Button className={isGroupsChosen ? "bg-slate-400" : "bg-slate-700"} 
+            onPress={() => notifySetGroupsChosen(false)}>
+          <ButtonIcon as={Rows4}/>
+          <ButtonText>Posts</ButtonText>
+        </Button>
+        <Button className={!isGroupsChosen ? "bg-slate-400" : "bg-slate-700"} 
+            onPress={() => notifySetGroupsChosen(true)}>
+          <ButtonIcon as={Users}/>
+          <ButtonText>Groups</ButtonText>
+        </Button>
+     </HStack>
+    </Center>
   );
 };
