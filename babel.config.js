85b1bf82d02bcb41edf2962eba47ef3f1bd4093c
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-01-17 09:48:51 +0800

    feat(group): Integrate Gluestack

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(false);
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ["module:metro-react-native-babel-preset", "nativewind/babel"],
     plugins: [
       "jest-hoist",
       [
