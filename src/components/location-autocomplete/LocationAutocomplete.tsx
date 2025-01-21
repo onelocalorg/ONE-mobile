f588edfb79eeca601b9f4118bb4e3ef357358ddb
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-01-21 10:40:00 +0800

    fix(event): VirtualizedList warning

--- src/components/location-autocomplete/LocationAutocomplete.tsx
+++ src/components/location-autocomplete/LocationAutocomplete.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { ScrollView } from "react-native";
 import {
   GooglePlaceData,
   GooglePlaceDetail,
@@ -28,58 +29,72 @@ export const LocationAutocomplete = ({
   }, [address]);
 
   return (
-    <GooglePlacesAutocomplete
-      ref={ref}
-      styles={{
-        // container: {
-        //   flex: 1,
-        //   flexDirection: "column",
-        // },
-        textInput: {
-          backgroundColor: "#E8E8E8",
-          height: 35,
-          borderRadius: 10,
-          color: "black",
-          fontSize: 14,
-          borderColor: theme.colors.black,
-          borderWidth: theme.borderWidth.borderWidth1,
-        },
-        // listView: {
-        //   color: "black", //To see where exactly the list is
-        //   backgroundColor: "red",
-        //   zIndex: 10000000, //To popover the component outwards
-        //   // position: 'absolute',
-        //   // top: 45
-        // },
-        row: {
-          backgroundColor: "white",
-          width,
-        },
-        predefinedPlacesDescription: {
-          color: "black",
-        },
-        description: {
-          color: "black",
-          fontSize: 14,
-        },
+    <ScrollView
+      horizontal
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      listViewDisplayed={false}
-      textInputProps={{
-        placeholderTextColor: "gray",
-      }}
-      placeholder={placeholder}
-      GooglePlacesDetailsQuery={{ fields: "geometry" }}
-      fetchDetails={true}
-      onPress={(data, details) => {
-        LOG.debug("GooglePlacesAutocomplete", details?.geometry.location);
+      scrollEnabled={false}
+      keyboardShouldPersistTaps="handled"
+    >
+      <GooglePlacesAutocomplete
+        ref={ref}
+        styles={{
+          container: {
+            flex: 1,
+            flexDirection: "column",
+            zIndex: 10000000,
+          },
+          textInput: {
+            backgroundColor: "#E8E8E8",
+            height: 35,
+            borderRadius: 10,
+            color: "black",
+            fontSize: 14,
+            borderColor: theme.colors.black,
+            borderWidth: theme.borderWidth.borderWidth1,
+          },
+          // listView: {
+          //   color: "black", //To see where exactly the list is
+          // zIndex: 10000000, //To popover the component outwards
+          //   // position: 'absolute',
+          //   // top: 45
+          // },
+          row: {
+            backgroundColor: "white",
+            zIndex: 10000000, //To popover the component outwards
+            width,
+          },
+          predefinedPlacesDescription: {
+            color: "black",
+          },
+          description: {
+            color: "black",
+            fontSize: 14,
+          },
+        }}
+        keyboardShouldPersistTaps="handled"
+        disableScroll={true}
+        listViewDisplayed={false}
+        textInputProps={{
+          placeholderTextColor: "gray",
+        }}
+        placeholder={placeholder}
+        GooglePlacesDetailsQuery={{ fields: "geometry" }}
+        fetchDetails={true}
+        onPress={(data, details) => {
+          LOG.debug("GooglePlacesAutocomplete", details?.geometry.location);
 
-        onPress(data, details);
-      }}
-      query={{
-        key: process.env.GOOGLE_API_KEY, // client
-        language: "en",
-      }}
-      currentLocationLabel="Current location"
-    />
+          onPress(data, details);
+        }}
+        query={{
+          key: process.env.GOOGLE_API_KEY, // client
+          language: "en",
+        }}
+        currentLocationLabel="Current location"
+      />
+    </ScrollView>
   );
 };
