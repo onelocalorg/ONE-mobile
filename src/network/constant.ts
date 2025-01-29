0375dbd3e83d6626a5e0c0ce0fcc64a9f0a290b6
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-01-29 13:45:34 +0700

    chore: Bump build to 8.3.3

--- src/network/constant.ts
+++ src/network/constant.ts
@@ -42,5 +42,5 @@ export const apiKeys = {
   packageDetail: "packageDetails",
 };
 
-export const IOS_VERSION = "8.3.2";
-export const ANDROID_VERSION = "8.3.2";
+export const IOS_VERSION = "8.3.3";
+export const ANDROID_VERSION = "8.3.3";
