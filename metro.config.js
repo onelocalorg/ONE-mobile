7a19533c45cdf3aec56305866a88a29265e08fbf
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-02-20 18:21:23 +0530

    fix: Update package-lock

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,18 @@
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
+const { withSentryConfig } = require("@sentry/react-native/metro");
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-
-const {
- withSentryConfig
-} = require("@sentry/react-native/metro");
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://metrobundler.dev/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  cacheVersion: process.env.NODE_ENV,
-});
+const config = {};
 
-module.exports = withNativeWind(withSentryConfig(config), 
-  { input: './global.css', inlineRem: 16 });
+module.exports = withSentryConfig(
+  withNativeWind(mergeConfig(getDefaultConfig(__dirname), config), {
+    input: "./global.css",
+    inlineRem: 16,
+  })
+);
